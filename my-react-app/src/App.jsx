52290788544241c962db83aa5f72bcf5ebfe819c
Amulya B <amulyaboppana54@gmail.com>
2025-03-04 11:32:33 +0530

    update

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Amulya B</h1>
+        <p><strong>Email:</strong> my.email@example.com</p>
+        <p><strong>Phone:</strong> +1234567890</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
